5d36013ac207e555d8ca677355e9dd144ff317b0
Roman Nazarenko <r.nazarenko.81@gmail.com>
2020-10-11 21:39:38 +0300

    add animation to mobile menu, move the functionality that is responsible for hiding menu from _row.css into _site-header.css module

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,18 @@
+class MobileMenu {
+	constructor(){
+		this.menuIcon = document.querySelector('.site-header__menu-icon')
+		this.menuContent = document.querySelector('.site-header__menu')
+		this.siteHeader = document.querySelector('.site-header')
+	}
+
+	fireEvents(){
+		this.menuIcon.addEventListener('click', () => this.toggleMenu())
+	}
+
+	toggleMenu(){
+		this.menuContent.classList.toggle('site-header__menu--is-visible')
+		this.siteHeader.classList.toggle('site-header--background')
+	}
+}
+
+export default MobileMenu
