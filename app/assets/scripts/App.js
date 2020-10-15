5038ca17a688d0916881eaff0caa0c4536ceb2b2
Roman Nazarenko <r.nazarenko.81@gmail.com>
2020-10-15 20:38:58 +0300

    complete modal functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
 if(module.hot) module.hot.accept()
 
@@ -9,6 +10,7 @@ const mobileMenu = new MobileMenu()
 const stickyHeader = new StickyHeader()
 const revealOnScroll = new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
 const revealOnScroll2 = new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
+new Modal()
 
 mobileMenu.fireEvents()
 stickyHeader.fireEvents()
