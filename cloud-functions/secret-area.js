414a615617793998a6b4bb1db3f5eb38c8db3687
Roman Nazarenko <r.nazarenko.81@gmail.com>
2020-10-21 19:21:26 +0300

    update cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,18 @@
 exports.handler = function(event, context, callback) {
-	callback(null, {
-		statusCode: 200,
-		body: 'Welcome to the super secret area'
-	})
+	const secretContent = `
+	<h3>Welcome to the Secret Area</h3>
+	<p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four<p>
+	`
+	let body 
+	event.body ? body = JSON.parse(event.body) : body = {}
+	if(body.password == 'javascript'){
+		callback(null, {
+			statusCode: 200,
+			body: secretContent
+		})
+	} else {
+		callback(null, {
+			statusCode: 401
+		})
+	}
 }
